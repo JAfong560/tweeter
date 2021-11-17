16dfc6530a0f0cf90d65c5bb46aefab0b41fbde5
Jonathan Afong <jonny560a@hotmail.com.com>
2021-11-17 17:09:06 -0500

    Creating tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
     // --- our code goes here ---
-    $("#btn").on('click', function() {
-        console.log(this); //The this keyword is a reference to the button
-    });
 
     // target the area on the DOM with jQuery
     const textarea = $("#tweet-text")
@@ -14,10 +11,16 @@ $(document).ready(function() {
     console.log(output);
 
     textarea.on('keyup', function () {
-        console.log(textstring.value);
         let tweetText = textstring.value
         let counter = output[0];
-        console.log(tweetText.length);
+
+        if (counter.value > 140) {
+            output.css("color", "red");
+        }
+        if (counter.value < 140) {
+            output.css("color", "black");
+        }
+        
         counter.value = tweetText.length;
     });
 });

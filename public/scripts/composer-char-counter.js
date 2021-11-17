84351d5707f3510a116509bea374750459883206
Jonathan Afong <jonny560a@hotmail.com.com>
2021-11-17 01:42:12 -0500

    Add tweet char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     // --- our code goes here ---
-  });
+    $("#btn").on('click', function() {
+        console.log(this); //The this keyword is a reference to the button
+    });
+
+    // target the area on the DOM with jQuery
+    const textarea = $("#tweet-text")
+    const textstring = textarea[0];
+    const output = $(".counter");
+    
+    console.log(textarea);
+    console.log(textstring);
+    console.log(output);
+
+    textarea.on('keyup', function () {
+        console.log(textstring.value);
+        let tweetText = textstring.value
+        let counter = output[0];
+        console.log(tweetText.length);
+        counter.value = tweetText.length;
+    });
+});

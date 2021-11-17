4a686eba259c6cc9c2f805b55c882578b823278d
Jonathan Afong <jonny560a@hotmail.com.com>
2021-11-17 18:46:20 -0500

    Dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,5 +6,76 @@
 
 $(document).ready(function() {
     // --- our code goes here ---
+    console.log(timeago.format(new Date()));
+    // Test / driver code (temporary). Eventually will get this from the server.
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+        },
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        }
+      ]
     
+    function renderTweets(data) {
+      $('#tweets-container').empty();
+      data.forEach( (tweet) => {
+        console.log("Here is my tweet",tweet);
+        $('#tweets-container').prepend(createTweetElement(tweet));
+      })
+    }
+
+    function createTweetElement(tweetData) {
+    const tweet = tweetData.content.text;
+    const name = tweetData.user.name;
+    const avatarsImg = tweetData.user.avatars.small;
+    const tweeHandle = tweetData.user.handle;
+
+    const $createAvatar = $('<img>').addClass("avatar").attr("src",avatarsImg);
+    const $createName = $('<div>').addClass("tweeterName").text(name);
+    const $createTweet = $('<div>').addClass("tweeBody").text(tweet);
+    const $createHandle = $('<p>').addClass("userTwee").text(tweeHandle);
+
+    const $iconContainer = $("<div>").addClass("icons");
+    const $flag = $("<i>").addClass("fas fa-flag");
+    const $retweet = $("<i>").addClass("fas fa-retweet");
+    const $heart = $("<i>").addClass("fas fa-heart");
+
+    const $tweetData = $("<article>").addClass("tweet");
+    const $header = $("<header>");
+    const $footer = $("<footer>");
+    const $content = $("<div>").addClass("content");
+    const $dateOfTweet = $("<h6>").text(tweetData['created_at']);
+
+    $iconContainer.append($flag, $retweet, $heart);
+    $header.append($createAvatar, $createName, $createHandle);
+    $content.append($createTweet);
+    $footer.append($iconContainer, $dateOfTweet);
+    $tweetData.append($header, $content, $footer);
+
+
+    return $tweetData;
+    }
+
+    const $tweet = renderTweets(data);
+
+    // Test / driver code (temporary)
+    console.log($tweet); // to see what it looks like
+    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
